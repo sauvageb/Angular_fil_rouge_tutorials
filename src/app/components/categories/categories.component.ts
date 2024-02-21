fe1696b19a61ab4d3be5f30ae2c98c132d33c93a
sauvageb <sauvageboris.pro@gmail.com>
2024-02-21 17:48:24 +0100

    Revert "add types"
    This reverts commit 3d4b9dbe294783a5ce3a7a593a9817133926fe1c.

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,9 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {NgForOf} from "@angular/common";
 import {MatActionList, MatListItem} from "@angular/material/list";
 import {MatCard} from "@angular/material/card";
-import {CategoriesService} from "../../services/categories.service";
-import {Category} from "../models/category";
+import {Category} from "../../models/category";
 
 @Component({
   selector: 'app-categories',
@@ -17,16 +16,20 @@ import {Category} from "../models/category";
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
-export class CategoriesComponent implements OnInit {
+export class CategoriesComponent {
 
-  categories!: Category[];
-
-  constructor(private categoriesService: CategoriesService) {
-  }
-
-  ngOnInit(): void {
-    this.categories = this.categoriesService.getAll();
-  }
-
+  categories: Category[] = [
+    {id: 1, name: "Java"},
+    {id: 2, name: "Javascript"},
+    {id: 3, name: "Typescript"},
+    {id: 4, name: "PHP"},
+    {id: 5, name: "C#"},
+    {id: 6, name: "Angular"},
+    {id: 7, name: "Ruby"},
+    {id: 8, name: "Python"},
+    {id: 9, name: "C++"},
+    {id: 10, name: "Rust"},
+    {id: 11, name: "VBA"},
+  ];
 
 }
