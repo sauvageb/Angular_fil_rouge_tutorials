70268fc87cc5ea7081e8fb7ab09e73e779486e9d
sauvageb <sauvageboris.pro@gmail.com>
2024-02-21 07:50:05 +0100

    display categories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CategoriesComponent} from "./components/categories/categories.component";
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CategoriesComponent, MatToolbar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
+import {MatActionList, MatListItem} from "@angular/material/list";
+import {MatCard} from "@angular/material/card";
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    MatActionList,
+    MatCard,
+    MatListItem
+  ],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
 export class CategoriesComponent {
+
+  categories: any[] = [
+    {id: 1, name: "Java"},
+    {id: 2, name: "Javascript"},
+    {id: 3, name: "Typescript"},
+    {id: 4, name: "PHP"},
+    {id: 5, name: "C#"},
+    {id: 6, name: "Angular"},
+    {id: 7, name: "Ruby"},
+    {id: 8, name: "Python"},
+    {id: 9, name: "C++"},
+    {id: 10, name: "Rust"},
+    {id: 11, name: "VBA"},
+  ];
 
 }
