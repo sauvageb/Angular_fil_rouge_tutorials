6618d55c6756f09d1a36ff779bb4895d3b2eade1
sauvageb <sauvageboris.pro@gmail.com>
2024-02-22 11:12:47 +0100

    change url in development

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // Careful, must NOT end with /
-  BASE_API_URL: 'http://localhost:8080/api'
+  BASE_API_URL: 'https://backend-fil-rouge-tutorials.onrender.com/api'
 };
